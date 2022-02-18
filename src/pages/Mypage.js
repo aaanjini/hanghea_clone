4889c14474eb9e2496c85c8f5abacb55b652f30b
sojh93 <36619557+sojh93@users.noreply.github.com>
2022-02-18 16:51:44 +0900

    #1 브랜치 만들기용

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 const Mypage = () => {
     return(
-        <React.Fragment></React.Fragment>
+        <React.Fragment>
+
+
+        </React.Fragment>
     );
 }
 
