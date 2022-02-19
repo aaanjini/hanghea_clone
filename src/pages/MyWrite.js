b368bbb064c3edf6a775d474f94519ec9173642b
sojh93 <36619557+sojh93@users.noreply.github.com>
2022-02-19 15:58:22 +0900

    mypage, mypage edit 수정중

--- src/pages/MyWrite.js
+++ src/pages/MyWrite.js
@@ -1,17 +1,27 @@
 import React from "react";
 import {Image, Grid, Button} from "../elements/Index";
-import Text from "../components/Card";
+
 
 const MyWrite = (props) => {
     return(
         <React.Fragment>
-            <Grid>
+            <Button is_flex width="18%" margin="20px 20px 20px 300px" > 완료</Button>
             <Image is_flex size="150" shape="circle" style={{margin:"auto"}}/>
-            </Grid>
+            
             <Button is_flex width="30%" margin="auto" >프로필 사진 변경</Button>
+            <div style={{margin:"30px 0 20px 38px"}}>
             닉네임
-            <Text/>
+            </div>
+            <label>
+            <input type="text" name="nickname" style={{display:"block", margin:"auto", width:"80%"}}/>
+            </label>
 
+            <div style={{margin:"30px 0 20px 38px"}}>
+            소개
+            </div>
+            <label>
+            <input type="text" name="nickname" style={{display:"block", margin:"auto", width:"80%", height:"10vh", paddingTop:"0", paddingBottom:"0"}}/>
+            </label>
         </React.Fragment>
     );
 }
