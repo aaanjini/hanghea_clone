1f31f467f368f5a07a5a4d40a23887419b015f70
aaanjini <zini6633@gmail.com>
2022-02-19 13:33:38 +0900

    깃 병합 오류 수정완료

--- src/elements/Index.js
+++ src/elements/Index.js
@@ -8,5 +8,8 @@ import MobileView from "./MobileView";
 import Image from "./Image";
 import VeiwContent from "./VeiwContent";
 import CommentInput from "./CommentInput";
+import CardMyInfo from "./CardMyInfo";
+import ButtonMyInfo from "./ButtonMyInfo";
+import MobileViewMyInfo from "./MobileViewMyInfo";
 
-export {Grid, Input, Button, Text, Image, Tag, TagGrid, MobileView, VeiwContent,CommentInput} ;
+export {Grid, Input, Button, Text, Image, Tag, TagGrid, MobileView, VeiwContent,CommentInput, MobileViewMyInfo, CardMyInfo, ButtonMyInfo} ;
--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,9 +1,53 @@
 import React from "react";
+import { ButtonMyInfo, MobileViewMyInfo, Grid, CardMyInfo, Image}  from "../elements/Index";
+import styled from "styled-components";
+import Header from "../components/Header";
+import Card from "../components/Card";
+// import { useSelector, useDispatch } from "react-redux";
+// import { actionCreators as MyActions } from "../redux/modules/mypage";
 
-const Mypage = () => {
+
+const Mypage = (props) => {
+
+//   const dispatch = useDispatch();
+//   const post_list = useSelector((state) => state.mypage.my_list);
+//   console.log(post_list);
+
+//   React.useEffect(() => {
+//     dispatch(MyActions.myPostDB());
+//   }, []);
+
     return(
-        <React.Fragment></React.Fragment>
+        <React.Fragment>
+            <Header text="내정보"/>            
+            <Grid  margin="50px 0 70px" height="calc(100% - 121px)" is_scroll>
+                <Grid padding="20px" is_flex>
+                    <Image size="100"  shape="circle"/>
+                    <Grid>
+                        닉네임,소개글
+                    </Grid>
+                    <ButtonMyInfo style={{textAlign: "right", margin: "50px 50px 50px 0"}}/>
+                </Grid>
+                <Grid padding="16px 8px">
+                    <Card/><Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                </Grid>
+                
+            </Grid>
+        </React.Fragment>
     );
 }
+
+const Line = styled.hr`
+    border-color: black;
+    padding: 15px;
+    margin: 16px;
+`
+
+
 
 export default Mypage;
